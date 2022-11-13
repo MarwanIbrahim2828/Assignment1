29144aceab2ce270e32aa897c526b0e143f9cc25
MarwanIbrahim2828 <marwan.2828.i@gmail.com>
2022-11-13 18:31:06 +0200

    we

--- main.py
+++ main.py
@@ -7,13 +7,3 @@ print(y,"-->",t)
 
 age=input("Enter your age ->")
 if age<20:
-<<<<<<< Updated upstream
-    print("teen")
-=======
-    print("teen")
-
-a=50
-c=60
-d=66
-
->>>>>>> Stashed changes
